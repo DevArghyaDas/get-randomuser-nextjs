94d34af890e026e54f34811cd346f464d54322f0
Arghya Das <mrarghyadasdev@gmail.com>
2025-07-05 09:32:32 +0530

    env var modified

--- src/app/users/[slug]/page.tsx
+++ src/app/users/[slug]/page.tsx
@@ -14,7 +14,7 @@ type Params = Promise<{ slug: string }>;
 const page = async ({ params }: { params: Params }) => {
   const numberOfUsers: string = (await params).slug;
 
-  const baseUrl = process.env.API_URL as string;
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL as string;
 
   if (parseInt(numberOfUsers) <= 30 && parseInt(numberOfUsers) > 0) {
     const { data } = await axios.get(baseUrl, {
